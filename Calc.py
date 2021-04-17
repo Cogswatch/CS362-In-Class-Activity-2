558b6f4a8dda8c36495506327cc971c3a4665263
C. Greyston Brady <c.greyston.brady@gmail.com>
2021-04-16 18:18:17 -0700

    Added other calculation types

--- Calc.py
+++ Calc.py
@@ -1,4 +1,16 @@
 def calcAdd(a, b):
   return a + b
 
+def calcSub(a, b):
+  return a - b
+
+def calcMul(a, b):
+  return a * b
+
+def calcDiv(a, b):
+  return a / b
+
 print(calcAdd(3,6))
+print(calcSub(3,6))
+print(calcMul(3,6))
+print(calcDiv(3,6))
